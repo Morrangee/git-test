aa68f8259e8f3b652d5d640785b533f557f8c548
Morrangee <776160839@qq.com>
2023-01-27 18:25:38 +0800

    second commit

--- src/main/java/com/aaa/Main.java
+++ src/main/java/com/aaa/Main.java
@@ -2,6 +2,9 @@ package com.aaa;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello git!");
+
+        System.out.println("Hello world!");
     }
 }
